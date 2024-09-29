ea4f60747f748b18282d1fee278c9e89733fc4f8
squarebh <mcheng60@stuy.edu>
2024-09-29 16:41:55 -0400

    Added function to concatenate arrays as well as test cases for the method

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -7,6 +7,9 @@ public class MyArrays{
 	System.out.println("Address for original array: " + anArray);
 	System.out.println("address for copy array: " + returnCopy(anArray));
 	System.out.println("Check for equal addresses: " + (anArray == returnCopy(anArray)));
+	int[] secondArray = new int[] {10,56,124,22,421};
+	int[] secondTestArray = new int[] {56,78,129,20,1,1,1};
+	System.out.println("Result for concatenation of two arrays: " + arrayToString(concatArray(secondArray,secondTestArray)));
   }
   public static String arrayToString(int[] nums)
   {
@@ -30,6 +33,20 @@ public class MyArrays{
 		}
 		return copy;
 	}
+	public static int[] concatArray(int[] ary1, int[] ary2)
+	{
+		int[] newArray = new int[ary1.length + ary2.length];
+		for (int i = 0; i < newArray.length;i++)
+		{
+			if ( i < ary1.length)
+			{
+				newArray[i] = ary1[i];
+			}
+			if ( i >= ary1.length)
+			{
+				newArray[i] = ary2[i - ary1.length];
+			}
+		}
+		return newArray;
+	}
 }
-
-
