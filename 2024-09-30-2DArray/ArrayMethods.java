031b57b1121835967f9f2d0685ed13a322e3e0ec
squarebh <mcheng60@stuy.edu>
2024-10-01 20:19:27 -0400

    Added methods for ReplaceNegative and copying the arrays.Also added the test cases

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -68,6 +68,60 @@ public class ArrayMethods{
 	}
 	return secondArray;
   }
+      
+        //3. Modify a given 2D array of integer as follows:
+    //Replace all the negative values:
+    //-When the row number is the same as the column number replace
+    //that negative with the value 1
+    //-All other negatives replace with 0
+  public static int[][] replaceNegative(int[][] vals){
+  for (int i = 0; i<vals.length;i++){
+    for(int j = 0; j<vals[i].length;j++){
+      if (0>vals[i][j]){
+        vals[i][j]=0;
+        if(i == j){
+          vals[i][j]=1;
+        }
+      }
+    }
+  }
+    return vals;
+  }
+
+    //4. Make a copy of the given 2d array.
+    //When testing : make sure that changing the original does NOT change the copy.
+    //DO NOT use any built in methods that "copy" an array.
+    //You SHOULD write a helper method for this.
+    //If you don't see a good way to do that, you should stop and look at prior methods.
+  public static int[][] copy(int[][] nums){
+	  int rows = nums.length;
+	  int columns = 0;
+	  for (int i = 0; i < nums.length ; i++)
+	  {
+		  if (nums[i].length > columns)
+		  {
+			  columns = nums[i].length;
+		  }
+	  }
+	  int[][] newArray = new int[rows][columns];
+	  for ( int i = 0; i < rows; i ++)
+	  {
+		  int index = nums[i].length;
+		  if ( columns > index)
+			  {
+				  newArray[i] = new int[index];
+			  }
+		  for (int x = 0; x < columns; x ++)
+		  {
+			  if ( x > index - 1)
+			  {
+				  break;
+			  }
+			  newArray[i][x] = nums[i][x];
+		  }
+	  }
+    return newArray;//placeholder so it compiles
+  }
   public static void main (String[]args){
     int[][] ary = new int[3][5];
     System.out.println(arrToString(ary));
@@ -75,5 +129,9 @@ public class ArrayMethods{
 	System.out.println(arr2DSum(secondAry));
 	int[][] testingArray = new int[][] {{1,2,3},{4,5,6}};
 	System.out.println(arrToString(swapRC(testingArray)));
+	int[][] testNegative = new int[][] {{-2,-3,-1},{-1,-1,-2}};
+	System.out.println(arrToString(replaceNegative(testNegative)));
+	int[][] testingCopy = new int[][]{{1,2,3,4},{1,2,3,4,5,6,7}};
+	System.out.println(arrToString(copy(testingCopy)));
   }
 }
