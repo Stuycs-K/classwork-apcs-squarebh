97d4e98704aa4c84314fca001ba3fa00eec2c35b
Matthew Cheng <mcheng60@stuy.edu>
2024-10-07 11:33:52 -0400

    Added 2D Array to string method

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -24,7 +24,14 @@ public class ArrayDemo{
   //as long as the parameters are different! (type and/or quantity must be different)
   //Pro tip: you should be using your 1D arrToString in this method!
   public static String arrToString(int[][]ary){
-    return "";
+    String str = "[";
+    for (int i = 0; i < ary.length; i++){
+        str += arrToString(ary[i]);
+        if(i<ary.length - 1){
+          str+= ", ";
+        }
+    }
+    return str + "]";
   }
 
   //1. Calculate and return how many elements equal zero in the 2D array.
