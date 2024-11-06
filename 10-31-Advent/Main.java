6ddd5dbcd577a93d1b765a7a82767dbb42fdc3c7
squarebh <mcheng60@stuy.edu>
2024-11-05 22:43:54 -0500

    Added part A as well as 1 test case

--- 10-31-Advent/Main.java
+++ 10-31-Advent/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+  @SuppressWarnings("resource")
   public static void main (String[] args) {
 
     Scanner sc = new Scanner("Some String (or File) Here");
--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,1 +1,41 @@
-public static void
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TriangleTester {
+  public static void main (String[] args) {
+    //System.out.println(isTri(5, 5, 2));
+    System.out.println(countTrianglesA("inputTri.txt"));
+}
+
+public static int countTrianglesA(String filename) {
+    try{
+    int counter = 0;
+    int x, y, z = 0;
+    File file = new File(filename);//1
+    Scanner input = new Scanner(file);
+    while(input.hasNextLine() && input.hasNextInt()){
+        
+        x = input.nextInt();
+        y = input.nextInt();
+        z = input.nextInt();
+        input.nextLine();
+      if (isTri(x, y, z)) {
+          counter += 1;
+      }
+    }
+    input.close();
+    return counter;
+}catch (FileNotFoundException ex) {
+    //File not found what should you do?
+    System.out.println("File not found");
+    return -1; //you can return from a void function just don't put a value.
+  }
+
+}
+
+  public static boolean isTri(int a, int b, int c) {
+    return (a + b > c && a + c > b && b + c > a);
+  }
+
+}
