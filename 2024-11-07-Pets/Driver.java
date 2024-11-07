9527942572183c42719e7a0485b5e6e423645898
Matthew Cheng <mcheng60@stuy.edu>
2024-11-07 11:44:33 -0500

    We overwrote the getName and we wrote what happened in the first three lines

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,5 @@
+//getName was overwritten in the Bird class, so when it is called by a Bird Object, it returns "The MIGHT name".
+//However, when an Animal object calls getName(). it doesn't nto get affected by the overwritten getName() in the Bird class.
 public class Driver{
 
   public static void main (String[] args) {
