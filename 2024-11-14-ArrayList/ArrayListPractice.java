2283af9595a6938a036a9311439245f2f6ad67ad
Matthew Cheng <mcheng60@stuy.edu>
2024-11-14 11:45:15 -0500

    Added replaceEmpty and added test cases

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -15,9 +15,22 @@ public class ArrayListPractice{
     }
     return res;
   }
+  public static void replaceEmpty( ArrayList<String> original){
+  //Modify the ArrayList such that it has all of the empty strings are
+  //replaced with the word "Empty".
+  for (int i = 0; i < original.size();i++)
+  {
+    if(original.get(i).equals(""))
+    {
+      original.set(i, "Empty");
+    }
+  }
+}
   public static void main(String[] args)
   {
+    ArrayList<String> test1 = createRandomArray((int)(12*Math.random()));
     System.out.println(createRandomArray(5));
-
+    replaceEmpty(test1);
+    System.out.println(test1);
   }
 }
