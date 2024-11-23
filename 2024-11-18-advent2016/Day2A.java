49aa542a05b3c63a835b2b9e5bed1ff860997e0b
squarebh <mcheng60@stuy.edu>
2024-11-23 12:14:13 -0500

    Finished part 2 of day2A

--- 2024-11-18-advent2016/Day2A.java
+++ 2024-11-18-advent2016/Day2A.java
@@ -3,15 +3,15 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Day2A
 {
-  public static int getCode(String filename)
+  public static String getCode(String filename)
   {
     try{
       String[][] codeVals = new String[][] {{"0","0","1","0","0"},{"0","2","3","4","0"},{"5","6","7","8","9"},{"0","A","B","C","0"},{"0","0","D","0","0"}};
       
       String result = "";
       File file = new File(filename);//1
-      int currentRow = 1;
-      int currentColumn = 1;
+      int currentRow = 2;
+      int currentColumn = 0;
       Scanner input = new Scanner(file);
       String holdString = "";
       while(input.hasNextLine())
@@ -28,23 +28,44 @@ public class Day2A
             {
               currentRow -= 1;
             }
+            if ( codeVals[currentRow][currentColumn].equals("0"))
+            {
+              holdString = codeVals[currentRow + 1][currentColumn];
+              currentRow += 1;
+            }
+            else{
             holdString = codeVals[currentRow][currentColumn];
+            }
           }
           else if (hold.substring(i,i+1).equals("R"))
           {
-            if ( currentColumn != 2)
+            if ( currentColumn != 4)
             {
               currentColumn += 1;
             }
+            if ( codeVals[currentRow][currentColumn].equals("0"))
+            {
+              holdString = codeVals[currentRow][currentColumn - 1];
+              currentColumn -= 1;
+            }
+            else{
             holdString = codeVals[currentRow][currentColumn];
+            }
           }
           else if (hold.substring(i,i+1).equals("D"))
           {
-            if (!( currentRow == 2))
+            if (!( currentRow == 4))
             {
               currentRow += 1;
             }
+            if ( codeVals[currentRow][currentColumn].equals("0"))
+            {
+              holdString = codeVals[currentRow - 1][currentColumn];
+              currentRow -= 1;
+            }
+            else{
             holdString = codeVals[currentRow][currentColumn];
+            }
           }
           else if (hold.substring(i,i+1).equals("L"))
           {
@@ -52,18 +73,25 @@ public class Day2A
             {
               currentColumn -= 1;
             }
+            if ( codeVals[currentRow][currentColumn].equals("0"))
+            {
+              holdString = codeVals[currentRow][currentColumn+1];
+              currentColumn += 1;
+            }
+            else{
             holdString = codeVals[currentRow][currentColumn];
+            }
           }
         }
         result = result + holdString;
       }
       input.close();
-      return Integer.parseInt(result);
+      return result;
     }
 
     catch (FileNotFoundException e) {
       System.out.println("File not found");
-      return -1;
+      return "";
    }
   }
   public static void main(String[] args){
