717aac3346daabafa5e644158e3ef975c7376da5
squarebh <mcheng60@stuy.edu>
2024-12-06 11:38:16 -0500

    Added the methods in warrior for the first few. Instantiated in driver. Added adventurer class

---
+++ 2024-12-06-Adventurer/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+    public static void main(String[] args){
+        Adventurer p1 = new Warrior ("Jerry", 10);
+        
+    }
+    
+}
---
+++ 2024-12-06-Adventurer/Warrior.java
@@ -0,0 +1,51 @@
+public class Warrior extends Adventurer{
+    private int rage, maxRage;
+    public Warrior(String name)
+    {
+        super(name);
+    }
+    public Warrior (String name, int hp)
+    {
+        super(name,hp);
+    }
+    public  String getSpecialName(){
+        return "rage";
+    }
+  //accessor methods
+  public  int getSpecial(){
+    return rage;
+  }
+  public  void setSpecial(int n){
+    rage = n;
+  }
+  public  int getSpecialMax(){
+    return maxRage;
+  }
+ 
+  /*
+    all adventurers must have a way to attack enemies and
+    support their allys
+  */
+  //hurt or hinder the target adventurer
+  
+  public  String attack(Adventurer other){
+    return "";
+  }
+
+  //heall or buff the target adventurer
+  public  String support(Adventurer other){
+    return "";
+  }
+
+  //heall or buff self
+  public  String support(){
+    return "";
+  }
+
+  //hurt or hinder the target adventurer, consume some special resource
+  public  String specialAttack(Adventurer other)
+  {
+    return "";
+  }
+
+}
