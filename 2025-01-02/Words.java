c9b8b01c98151a183383e6ce2a06c052940ac29b
Matthew Cheng <mcheng60@stuy.edu>
2025-01-03 11:39:55 -0500

    Added base case and main tester with test for the class

---
+++ 2025-01-02/Words.java
@@ -0,0 +1,15 @@
+public class Words{
+  public static void main(String[] args){
+    System.out.println("test");
+  }
+  public static void makeWords(int letters, String result, String alphabet){
+    if(letters == 0){
+      System.out.println(result);
+    }
+    else{
+      for (int i = 0; i < alphabet.length();i++){
+        makeWords()
+      }
+    }
+  }
+}
